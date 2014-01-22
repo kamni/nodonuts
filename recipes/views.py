7e55eaba781c474775f4040e8d29fa79d589ce47
Jay Leadbetter <jay.leadbetter@gmail.com>
2014-01-22 12:24:10 -0700

    Basic templates for the site and for recipes app

--- recipes/views.py
+++ recipes/views.py
@@ -2,6 +2,4 @@ from django.views.generic import TemplateView
 
 
 class Home(TemplateView):
-    def get(self, request, *args, **kwargs):
-        from django.http import HttpResponse
-        return HttpResponse('home')
+    template_name = "home.html"
