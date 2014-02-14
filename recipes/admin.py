1ec463ecf02b43a7a5b151381bd332c1b7e44141
Jay Leadbetter <jay.leadbetter@gmail.com>
2014-02-13 20:40:25 -0700

    [#5] added Rating to admin

--- recipes/admin.py
+++ recipes/admin.py
@@ -3,7 +3,11 @@ from django.contrib import admin
 from django.db import models
 from tinymce.widgets import TinyMCE
 
-from recipes.models import Recipe, RecipeTag
+from recipes.models import Rating, Recipe, RecipeTag
+
+
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ('recipe', 'rated_by', 'vote')
 
 
 class RecipeAdmin(admin.ModelAdmin):
@@ -11,6 +15,7 @@ class RecipeAdmin(admin.ModelAdmin):
     formfield_overrides = {models.TextField: {'widget': TinyMCE(attrs={'cols': 80, 
                                                                        'rows': 15})}}
 
-
+ 
+admin.site.register(Rating, RatingAdmin)    
 admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(RecipeTag)
