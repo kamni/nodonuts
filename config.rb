b62d3bdb6af1d11c93148635e10ab17c0f2bba3a
Jay Leadbetter <jay.leadbetter@gmail.com>
2014-02-21 20:08:01 -0700

    Added Foundation 5; rearranged scss layout

--- config.rb
+++ config.rb
@@ -1,24 +1,35 @@
-# Require any additional compass plugins here.
+add_import_path "static/bower_components/foundation/scss"
 
-# Set this to the root of your project when deployed:
-http_path = "/"
 css_dir = "static/css"
-sass_dir = "static/css/sass"
+sass_dir = "static/scss"
 images_dir = "static/img"
 javascripts_dir = "static/js"
 
+http_path = "/static/"
+http_images_path = "/static/img/"
+http_javascripts_path = "static/js/"
+
+# Change this variable to 'production' for compressed css files
+environment = :development
+
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
-output_style = :compressed
+if environment == :development
+  line_comments = true
+  output_style = :nested
+  sass_options = {:debug_info => true}
+else
+  line_comments = false
+  output_style = :compressed
+  sass_options = {:debug_info => false}
+end
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 relative_assets = true
 
-# To disable debugging comments that display the original location of your selectors. Uncomment:
-line_comments = false
 
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
 # preferred_syntax = :sass
 # and then run:
-# sass-convert -R --from scss --to sass static/css/sass scss && rm -rf sass && mv scss sass
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
