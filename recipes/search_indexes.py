3cca4e4e4ae37322bf1d0b48dfa9aba9f0e36d15
J Leadbetter <codemonkey@jleadbetter.com>
2014-02-23 17:52:08 -0700

    [#7, #10, #11] Search now works for tags, title, and ingredients

--- recipes/search_indexes.py
+++ recipes/search_indexes.py
@@ -4,19 +4,19 @@ from recipes.models import Recipe
 
 
 class RecipeIndex(indexes.SearchIndex, indexes.Indexable):
-    recipe_text = indexes.CharField(document=True, use_template=True)
-    added_by = indexes
-    #.CharField(model_attr='added_by')
-    popularity = indexes.DateTimeField(model_attr='popularity')
-    #is_public = indexes.
+    text = indexes.CharField(document=True, use_template=True)
+    tags = indexes.MultiValueField()
+    popularity = indexes.DecimalField(model_attr='popularity')
     
-    # filter by added_by, is_public
-    # search by text in title, ingredients
-    # search by tag
-    # filter by popularity
+    # TODO: filter by added_by, is_public -- do in a custom search class
 
     def get_model(self):
         return Recipe
 
     def index_queryset(self, using=None):
         return self.get_model().objects.filter(date_added__lte=datetime.datetime.now())
+    
+    def prepare(self, object):
+        self.prepared_data = super(RecipeIndex, self).prepare(object)
+        self.prepared_data['tags'] = [rtag.tag for rtag in object.tags.all()]
+        return self.prepared_data
