87460f9a58337ae9aca0192e631e307a4b45404e
J Leadbetter <codemonkey@jleadbetter.com>
2014-02-25 11:33:05 -0700

    More fixes to wsgi scripts

--- nodonuts/wsgi.py
+++ nodonuts/wsgi.py
@@ -10,4 +10,5 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nodonuts.settings")
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
