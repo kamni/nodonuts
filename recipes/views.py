d2f8e831d8318d3e26169b0de65676ee0b347d16
J Leadbetter <codemonkey@jleadbetter.com>
2014-02-27 11:51:55 -0700

    [#11] updating search results to return in order of popularity

--- recipes/views.py
+++ recipes/views.py
@@ -8,7 +8,31 @@ class Home(TemplateView):
 
 class RecipeSearch(FormView):
     def get(self, request, *args, **kwargs):
-        return SearchView()(request)
+        return RecipeSearchHelper()(request)
     
     def post(self, request, *args, **kwargs):
-        return SearchView()(request)
+        return RecipeSearchHelper()(request)
+    
+    '''
+    # available methods from FormMixin
+    get_initial(self)
+    get_prefix(self)
+    get_form_class(self)
+    get_form(self, form_class)
+    get_form_kwargs(self)
+    get_success_url(self)
+    form_valid(self, form)
+    form_invalid(self, form)
+    
+    # and don't forget
+    get_context_data(self, kwargs)
+    post_context_data(self, kwargs)
+    
+    context_data expected:
+       form, query, results
+    '''
+
+class RecipeSearchHelper(SearchView):
+    def get_results(self):
+        """ Overrides the parent method to sort results by popularity"""
+        return self.form.search().order_by('-popularity')
