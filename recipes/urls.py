b6b87d0390f1a6b560f8c2cdfb6e79f15fed247c
J Leadbetter <codemonkey@jleadbetter.com>
2014-03-05 19:00:38 -0500

    [#7, #9, #10, #11] refactoring search view

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, include, url
+from haystack.views import search_view_factory
 
 from recipes.views import *
 
 
 urlpatterns = patterns('',
     url(r'^$', Home.as_view(), name='home'),
-    url(r'^search/$', RecipeSearch.as_view(), name='recipe_search'),
+    url(r'^search/$', search_view_factory(view_class=RecipeSearchView), name='recipe_search'),
 )
--- recipes/views.py
+++ recipes/views.py
@@ -16,35 +16,11 @@ class Home(TemplateView):
                 'tags': RecipeTag.objects.filter_list(exclude_miscellaneous=True)}
     
 
-class RecipeSearch(TemplateView):
-    template_name = "search/search.html"
+class RecipeSearchView(SearchView):
+    """
+    TODO: docs and tests
+    """
     
-    def get(self, request, *args, **kwargs):
-        return RecipeSearchHelper()(request)
-    
-    def post(self, request, *args, **kwargs):
-        return RecipeSearchHelper()(request)
-    
-    '''
-    # available methods from FormMixin
-    get_initial(self)
-    get_prefix(self)
-    get_form_class(self)
-    get_form(self, form_class)
-    get_form_kwargs(self)
-    get_success_url(self)
-    form_valid(self, form)
-    form_invalid(self, form)
-    
-    # and don't forget
-    get_context_data(self, kwargs)
-    post_context_data(self, kwargs)
-    
-    context_data expected:
-       form, query, results
-    '''
-
-class RecipeSearchHelper(SearchView):
     def get_results(self):
         """Overrides the parent method to sort results by popularity"""
         return self.form.search().order_by('-popularity')
