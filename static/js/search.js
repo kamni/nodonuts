9c690fa48b0a42b187bfe955e290c93b3559a46f
J Leadbetter <codemonkey@jleadbetter.com>
2014-03-06 06:54:51 -0500

    [closes #10] toggle functionality for tags

--- static/js/search.js
+++ static/js/search.js
@@ -1,4 +1,12 @@
-function addTag(caller, tag) { /* Adds a tag to the query on the page */
-    $(caller).addClass("query");
-    $("#id_q").val($("#id_q").val() + ' "' + tag + '"');
+function toggleTag(caller, tag) { /* Adds or removes a tag to the query on the page */
+    query = $("#id_q").val();
+    quotedTag = '"' + tag + '"';
+    if (query.indexOf(quotedTag) >= 0) {
+        $("#id_q").val(query.replace(quotedTag, ""));
+        $(caller).removeClass("query");
+    }
+    else {
+        $("#id_q").val(query + " " + quotedTag);
+        $(caller).addClass("query");
+    }
 }
