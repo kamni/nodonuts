e8177cb2e181e0474e34fc8e0b31233ab1b12ae3
J Leadbetter <codemonkey@jleadbetter.com>
2014-03-07 17:35:02 -0500

    [closes #35] updated previous/next links on search to include serving size

--- recipes/forms.py
+++ recipes/forms.py
@@ -6,6 +6,7 @@ from recipes.models import ServingSize
 
 
 class RecipeSearchForm(SearchForm):
+    """TODO: docs and tests"""
     q = forms.CharField(required=False, label=_('Search'),
                         widget=forms.TextInput(attrs={'placeholder': 
                                                       'Enter search terms, ' +
@@ -18,6 +19,12 @@ class RecipeSearchForm(SearchForm):
                                        ('alphabetz', 'alphabetical (Z-A)')))
     ss = forms.ChoiceField(required=False, label="Serving Size",
                            choices=[(None, "-------")] + ServingSize.choices())
+    
+    def base_query_link(self):
+        """TODO: docs and tests"""
+        return "?q=%s&amp;order=%s&amp;ss=%s" % (self.q, self.order, 
+                                                 self.ss if self.ss is not None
+                                                 else '')
     
     def order_by(self, query, ordering=None):
         if ordering == 'newest':
@@ -34,88 +41,25 @@ class RecipeSearchForm(SearchForm):
         if not self.is_valid():
             return self.no_query_found()
         
-        qstring = self.cleaned_data.get('q')
-        ordering = self.cleaned_data.get('order')
+        self.q = self.cleaned_data.get('q')
+        self.order = self.cleaned_data.get('order')
         try:
-            srv_size = int(self.cleaned_data.get('ss'))
+            self.ss = int(self.cleaned_data.get('ss'))
         except ValueError:
-            srv_size = None
+            self.ss = None
         
-        if not (qstring or ordering or srv_size is not None):
+        if not (self.q or self.order or self.ss is not None):
             return self.no_query_found()
         
-        if qstring:
-            query = self.searchqueryset.auto_query(qstring)
+        if self.q:
+            query = self.searchqueryset.auto_query(self.q)
         else:
             query = self.searchqueryset.all()
             
-        if srv_size is not None:
-            query = query.filter(serving_size=srv_size)
+        if self.ss is not None:
+            query = query.filter(serving_size=self.ss)
         
         if self.load_all:
             query = query.load_all()
 
-        return self.order_by(query, ordering)
-
-
-'''
-q = forms.CharField(required=False, label=_('Search'))
-
-    def __init__(self, *args, **kwargs):
-        self.searchqueryset = kwargs.pop('searchqueryset', None)
-        self.load_all = kwargs.pop('load_all', False)
-
-        if self.searchqueryset is None:
-            self.searchqueryset = SearchQuerySet()
-
-        super(SearchForm, self).__init__(*args, **kwargs)
-
-    def no_query_found(self):
-        """
-        Determines the behavior when no query was found.
-
-        By default, no results are returned (``EmptySearchQuerySet``).
-
-        Should you want to show all results, override this method in your
-        own ``SearchForm`` subclass and do ``return self.searchqueryset.all()``.
-        """
-        return EmptySearchQuerySet()
-
-    def search(self):
-        if not self.is_valid():
-            return self.no_query_found()
-
-        if not self.cleaned_data.get('q'):
-            return self.no_query_found()
-
-        sqs = self.searchqueryset.auto_query(self.cleaned_data['q'])
-
-        if self.load_all:
-            sqs = sqs.load_all()
-
-        return sqs
-
-    def get_suggestion(self):
-        if not self.is_valid():
-            return None
-
-        return self.searchqueryset.spelling_suggestion(self.cleaned_data['q'])
-class ModelSearchForm(SearchForm):
-    def __init__(self, *args, **kwargs):
-        super(ModelSearchForm, self).__init__(*args, **kwargs)
-        self.fields['models'] = forms.MultipleChoiceField(choices=model_choices(), required=False, label=_('Search In'), widget=forms.CheckboxSelectMultiple)
-
-    def get_models(self):
-        """Return an alphabetical list of model classes in the index."""
-        search_models = []
-
-        if self.is_valid():
-            for model in self.cleaned_data['models']:
-                search_models.append(models.get_model(*model.split('.')))
-
-        return search_models
-
-    def search(self):
-        sqs = super(ModelSearchForm, self).search()
-        return sqs.models(*self.get_models())
-'''
+        return self.order_by(query, self.order)
