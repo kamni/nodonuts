66982dcb6e5238886f1690a4221a6c55996c64fa
J Leadbetter <codemonkey@jleadbetter.com>
2014-03-12 12:52:11 -0400

    [#36] javascript properly removes whitespace when removing tags from search

--- static/js/search.js
+++ static/js/search.js
@@ -2,12 +2,12 @@ function toggleTag(caller, tag) { /* Adds or removes a tag to the query on the p
     tagsInput = $("#id_tags")
     query = tagsInput.val();
     if (query.indexOf(tag) >= 0) {
-        tagsInput.val(query.replace(tag, ""));
+        tagsInput.val(query.replace(tag, "").replace(/\s+/g, ' ').replace(/(^\s+|\s+$)/g, ''));
         $(caller).removeClass("query");
     }
     else {
         if (query !== "") {
-            query = query + ", "
+            query = query + " "
         }
         tagsInput.val(query + tag);
         $(caller).addClass("query");
