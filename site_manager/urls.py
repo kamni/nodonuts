6df6173024c10fe5c9e56086300bee8debeac9fa
J Leadbetter <codemonkey@jleadbetter.com>
2014-03-14 19:12:33 -0400

    [#63] added a decorator to prevent going to admin panel as a non-admin

--- site_manager/urls.py
+++ site_manager/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.auth.decorators import user_passes_test
 
 from site_manager.views import *
 
 
+admin_only = user_passes_test(lambda user: user.is_superuser)
+
 urlpatterns = patterns('',
-    url(r'^$', AdminPanel.as_view(), name='admin_panel'),
+    url(r'^$', admin_only(AdminPanel.as_view()), name='admin_panel'),
 )
