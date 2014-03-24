033ee68db027634c8a93a3f14410889a3ff1563f
J Leadbetter <codemonkey@jleadbetter.com>
2014-03-23 21:19:46 -0600

    [#71] twitter avatar now displays

--- organizations/pipeline.py
+++ organizations/pipeline.py
@@ -1,3 +1,7 @@
+try:
+    import json
+except ImportError:
+    import simplejson as json
 from requests import request, HTTPError
 
 from django.core.files.base import ContentFile
@@ -10,8 +14,13 @@ def save_profile_image(strategy, user, response, details, *args, **kwargs):
     profile = UserProfile.objects.get_or_create(user=user)[0]
     
     is_new = kwargs.get('is_new', False) or not profile.avatar_url
-    if is_new and strategy.backend.name == 'facebook':
-        profile.avatar_url = 'http://graph.facebook.com/{0}/picture'.format(response['id'])
+    if is_new:
+        if strategy.backend.name == 'facebook':
+            profile.avatar_url = 'http://graph.facebook.com/{0}/picture'.format(response['id'])
+        elif strategy.backend.name == 'twitter':
+            if not response.get('default_profile_image'):
+                avatar_url = response.get('profile_image_url_https')
+                if avatar_url:
+                    avatar_url = avatar_url.replace('_normal.', '_bigger.')
+                    profile.avatar_url = avatar_url
         profile.save()
-    else:
-        pass
